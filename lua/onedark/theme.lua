c98469cfae4cf7562ee8d49298d49e73086c9869
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-11 19:50:22 +0530

    ✨ Fixed border color

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -32,7 +32,7 @@ function M.setup(config)
     -- TermCursor  = { }, -- cursor in a focused terminal
     -- TermCursorNC= { }, -- cursor in an unfocused terminal
     ErrorMsg = {fg = c.error}, -- error messages on the command line
-    VertSplit = {fg = c.bg1}, -- the column separating vertically split windows
+    VertSplit = {fg = c.bg}, -- the column separating vertically split windows
     Folded = {fg = c.blue, bg = c.fg_gutter}, -- line used for closed folds
     FoldColumn = {bg = c.bg, fg = c.fg_gutter}, -- 'foldcolumn'
     SignColumn = {bg = config.transparent and c.none or c.bg, fg = c.fg_gutter}, -- column where |signs| are displayed
