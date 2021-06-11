13d15dd705a5229814590d78a95eb57373dea8a4
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-11 18:41:58 +0530

    💖 Fix background highlight color & Format `colors.lua`
    Previously, `bg_highlight` set from the blue family. That only able
    to make contrast in `sidebar` windows, If `onedark_dark_sidebar` is
    set. This color tweak makes `bg_highlight` pop in light and dark
    background .
    
    Also, I formated the `lua/onedark/colors.lua` file with `luafmt`.

--- lua/onedark/colors.lua
+++ lua/onedark/colors.lua
@@ -14,10 +14,10 @@ function M.setup(config)
   colors = {
     none = "NONE",
     bg = "#282c34",
-    bg2 = '#21252b',
-    bg_visual = '#393f4a',
-    border = '#646e82',
-    bg_highlight = "#242b38",
+    bg2 = "#21252b",
+    bg_visual = "#393f4a",
+    border = "#646e82",
+    bg_highlight = "#2c313a",
     fg = "#abb2bf",
     fg_light = "#adbac7",
     fg_dark = "#798294",
@@ -29,12 +29,12 @@ function M.setup(config)
     orange = "#d19a66",
     yellow = "#e0af68",
     yellow2 = "#e2c08d",
-    bg_yellow = '#ebd09c',
+    bg_yellow = "#ebd09c",
     green = "#98c379",
     red = "#e86671",
     red1 = "#f65866",
-    git = { change = "#e0af68", add = "#109868", delete = "#9A353D", conflict = "#bb7a61" },
-    gitSigns = { change = "#e0af68", add = "#109868", delete = "#9A353D" },
+    git = {change = "#e0af68", add = "#109868", delete = "#9A353D", conflict = "#bb7a61"},
+    gitSigns = {change = "#e0af68", add = "#109868", delete = "#9A353D"}
   }
 
   util.bg = colors.bg
@@ -43,13 +43,13 @@ function M.setup(config)
     add = util.darken(colors.git.add, 0.15),
     delete = util.darken(colors.git.delete, 0.15),
     change = util.darken(colors.git.change, 0.15),
-    text = colors.fg_gutter,
+    text = colors.fg_gutter
   }
 
   colors.gitSigns = {
     add = util.brighten(colors.gitSigns.add, 0.2),
     change = util.brighten(colors.gitSigns.change, 0.2),
-    delete = util.brighten(colors.gitSigns.delete, 0.2),
+    delete = util.brighten(colors.gitSigns.delete, 0.2)
   }
 
   colors.git.ignore = colors.fg_gutter
